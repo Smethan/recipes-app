5f7d34521223486cf572187350e65ca2ce6c5393
AdShanks <aaronshanks3@gmail.com>
2023-02-07 14:28:45 -0600

    working on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 // import MainPage from './Containers/MainPage/MainPage.jsx';
 import FeatureRecipe from './Containers/FeatureRecipe/FeatureRecipe';
 import PopularRecipes from './Containers/PopularRecipes/PopularRecipes';
+import UserProfile from "./Containers/UserProfile/UserProfile"
 import Nav from "./Components/Nav/Nav"
-import { BrowserRouter as Router, Route, Swithc } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
   <Router>
-    <>
+    <div className="App">
       <Nav/>
-      <section>
-        <FeatureRecipe/>
-      </section>
-
-      <section>
-        <PopularRecipes/>
-      </section>
-    </>
+      <div className="body">
+        {/* <Routes> */}
+          {/* <Route exact path="/"> */}
+            <section>
+              <FeatureRecipe/>
+            </section>
+            
+            <section>
+              <PopularRecipes/>
+            </section>
+          {/* </Route> */}
+           {/* <Route  path="/profile">
+           <UserProfile/>
+          </Route> */}
+        {/* </Routes>  */}
+      </div>
+    </div>
   </Router>
     )
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import menuIcon from "../../Images/menu-icon.png";
 import userIcon from "../../Images/user-icon.png";
 
 const Nav = () => {
   return (
+    //Create Link for user icon image
     <div>
       <img src={menuIcon} alt="menu icon"  />
       <h2>_nology Recipes</h2>
-      <img src={userIcon} alt="user profile icon" width="75" height="75"   />
+      <Link to="/profile"> <img src={userIcon} alt="user profile icon" width="75" height="75"   /> </Link>
 
     </div>
   )
