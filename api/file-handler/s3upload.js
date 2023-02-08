3208b793c85773e59b3ba3d4147d30ad91f11563
smethan <ethanhargus@gmail.com>
2023-02-08 11:18:43 -0600

    multer fixed for now, still need to add AWS stuff

--- api/file-handler/s3upload.js
+++ api/file-handler/s3upload.js
@@ -1,5 +1,5 @@
 import multer from "multer";
-import { multer as multerS3 } from "multer-s3";
+import multerS3 from "multer-s3";
 import { S3Client } from "@aws-sdk/client-s3";
 
 const s3Config = {
