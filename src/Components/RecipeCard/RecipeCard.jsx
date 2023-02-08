14595620846598a6a7303cb33c330091ddb5d5f0
smethan <ethanhargus@gmail.com>
2023-02-08 15:49:10 -0600

    recipe card fixes

--- src/Components/RecipeCard/RecipeCard.jsx
+++ src/Components/RecipeCard/RecipeCard.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
- const RecipeCard = (props) => {
-  const {title, prep_time, cook_time, servings, ingredients, directions } = props.recipes
-  return (
-    <div className="RecidpeCard">
-      {/* <img src={image_url} alt= {name} height="300" width="100"/> */}
-      <h1>{title} </h1>
-      <p>Prep-time: {prep_time} </p>
-      <p>Cook Time: {cook_time} </p>
-      <p>Servings: {servings} </p>
-      <h2>Ingredients</h2>
-      <ul>
-        {ingredients.map(ingredients => {
-          return <li>{ingredients}</li>
-        })} 
-      </ul>
-      <h2>Directions</h2>
-      <ol>
-        {directions.map(direction => {
-          return <li> {directions} </li>
-        })}
-      </ol>
-    </div>
-  )
-}
+const RecipeCard = (props) => {
+	const { title, prep_time, cook_time, servings, ingredients, directions } = props.recipes;
+	return (
+		<div className="RecidpeCard">
+			{/* <img src={image_url} alt= {name} height="300" width="100"/> */}
+			<h1>{title} </h1>
+			<p>Prep-time: {prep_time} </p>
+			<p>Cook Time: {cook_time} </p>
+			<p>Servings: {servings} </p>
+			<h2>Ingredients</h2>
+			<ul>
+				{ingredients.map((ingredient) => {
+					return <li>{ingredient}</li>;
+				})}
+			</ul>
+			<h2>Directions</h2>
+			<ol>
+				{directions.map((direction) => {
+					return <li> {direction} </li>;
+				})}
+			</ol>
+		</div>
+	);
+};
 
-export default RecipeCard
+export default RecipeCard;
