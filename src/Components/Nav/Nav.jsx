34fd151f563acacdabc5a6a743b5c0bdfb08993e
Smethan <ethanhargus@gmail.com>
2023-02-09 10:21:34 -0600

    post merge fix

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 import menuIcon from "../../Images/menu-icon.png";
 import userIcon from "../../Images/user-icon.png";
 import Sidebar from "../Sidebar/Sidebar";
-import "./Nav.scss"
+import "./Nav.scss";
 
 const Nav = () => {
-  return (
-    <div className="navbar-container">
-      <div className="navbar">
-      <img src={menuIcon} className="ham" alt="menu icon" width="40" height="40"  />
-      <h4 className="small-link">About</h4>
-      <h2>_nology Recipes</h2>
-      <h4 className="small-link">All Recipes</h4>
-      <img src={userIcon} className="profile" alt="user profile icon" width="50" height="50"   />
-    </div>
-    </div>
-  )
-}
+	return (
+		<div className="navbar-container">
+			<div className="navbar">
+				<img src={menuIcon} className="ham" alt="menu icon" width="40" height="40" />
+				<h4 className="small-link">About</h4>
+				<h2>_nology Recipes</h2>
+				<h4 className="small-link">All Recipes</h4>
+				<img src={userIcon} className="profile" alt="user profile icon" width="50" height="50" />
+			</div>
+		</div>
+	);
+};
+
+export default Nav;
